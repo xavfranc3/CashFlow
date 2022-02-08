b813d68413c3b1479ccd6b89c51321ceea1a6600
Xav <xavfranc3@gmail.com>
2022-02-08 12:45:05 +0100

    Setup:Db connection & App bootstrap

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import 'dotenv/config';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import App from './app';
+import config from './ormconfig';
 import validateEnv from './utils/validateEnv';
 
 validateEnv();
 
-const app = new App([]);
-app.listen();
+(async () => {
+  try {
+    await createConnection(config);
+  } catch (error) {
+    console.log('Error while connecting to database', error);
+    return error;
+  }
+  const app = new App([]);
+  app.listen();
+})();
